c57a16558e43864e0f76b8ec986bccc8267b234d
Yuvraj <90456838+yuvraj279@users.noreply.github.com>
2023-06-21 18:22:28 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.java
+++ Minimum Platforms - GFG/minimum-platforms.java
@@ -48,29 +48,28 @@ class Solution
     static int findPlatform(int arr[], int dep[], int n)
     {
         // add your code here
-         Arrays.sort(arr); 
-        Arrays.sort(dep); 
-  
-        int plat_needed = 1, result = 1; 
-        int i = 1, j = 0; 
-  
-        while (i < n && j < n) { 
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int i=1; // Arraival Train
+        int j=0; // Departure Train
+        int count=1, result=1;
         
-            if (arr[i] <= dep[j]) { 
-                plat_needed++; 
-                i++; 
-            } 
-  
-            else if (arr[i] > dep[j]) { 
-                plat_needed--; 
-                j++; 
-            } 
-  
-            if (plat_needed > result) 
-                result = plat_needed; 
-        } 
-  
-        return result; 
+        while(i<n && j<n){
+            if(arr[i]<=dep[j]){
+                count++;
+                i++;
+            }
+            else if(arr[i]>dep[j]){
+                count--;
+                j++;
+            }
+            // Will store the max value here
+            if(count>result){
+                result=count;
+            }
+        }
+        return result;
+        
     }
     
 }
